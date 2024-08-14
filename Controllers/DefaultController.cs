1d3353b6ed507a828653d534b67df5b4d4fffd47
Nitin Bhakar <72448977+nbhakar@users.noreply.github.com>
2024-08-14 11:31:28 +0530

    Add files via upload

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index(int? id, int a=10, int b=20, int c=30)
+        {
+            //if (id == null)
+            //    return NotFound();
+            ViewBag.id = id;
+            ViewBag.a = a;
+            ViewBag.b = b;
+            ViewBag.c = c;
+
+
+            return View();
+            //return View("Index1");
+        }
+    }
+}
